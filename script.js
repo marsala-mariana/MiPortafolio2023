283ce3669531ca30865c9e3efb6d17710dc66f14
Mariana Marsala <stephaniemarsala08gmail.com>
2024-04-14 10:49:13 -0300

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+
 function seleccionar(link) {
   var opciones = document.querySelectorAll("#links  a");
   opciones[0].className = "";
@@ -8,13 +8,12 @@ function seleccionar(link) {
   opciones[4].className = "";
   link.className = "seleccionado";
 
-  //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
-  //en modo responsive
+
   var x = document.getElementById("nav");
   x.className = "";
 }
 
-//función que muestra el menu responsive
+
 function responsiveMenu() {
   var x = document.getElementById("nav");
   if (x.className === "") {
